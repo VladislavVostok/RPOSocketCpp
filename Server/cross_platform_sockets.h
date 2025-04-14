895c226111a0f8510e1942074928722567beea9e
Vladislav East <vladislav.sage@yandex.ru>
2025-04-14 21:25:51 +0300

    server

---
+++ Server/cross_platform_sockets.h
@@ -0,0 +1,57 @@
+#pragma once
+#ifndef CROSS_PLATFORM_SOCKETS
+#define CROSS_PLATFORM_SOCKETS
+
+using namespace std;
+
+
+#ifdef _WIN32
+	#include <WinSock2.h>
+	#include <WS2tcpip.h>
+	#pragma comment(lib, "ws2_32.lib")
+	#define close_socket closesocket
+	#define socklen_t int
+	#define SHUT_RDWR SD_BOTH
+	#define MSG_NOSIGNAL 0
+
+#else
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <unistd.h>
+	#include <arpa/inet.h>
+	#define close_socket close
+	#define INVALID_SOCKET -1
+	#define SOCKET_ERROR -1
+#endif
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <cstring>
+#include <stdexcept>
+
+
+
+inline void cleanup_network() {
+	#ifdef _WIN32
+		WSACleanup();
+	#endif
+}
+
+
+inline void check_socket_error(int result, const string& error_msg) {
+
+	if (result == SOCKET_ERROR) {
+	#ifdef _WIN32
+		int error = WSAGetLastError();
+		throw runtime_error(error_msg + ". Code error: " + to_string(error));
+	
+	#else
+		perror(error_msg.c_str());
+		throw runtime_error(error_msg);
+	#endif
+	}
+}
+
+#endif  //CROSS_PLATFORM_SOCKETS
---
+++ Server/server.cpp
@@ -0,0 +1,116 @@
+#include "cross_platform_sockets.h"
+#include "product.h"
+
+#include <sstream>
+
+using namespace std;
+
+map<int, Product> productDatabase = {
+
+	{ 1, {1, "Ноутбук Lenovo IdeaPad", 54999.99, 12, "Ноутбуки"} },
+	{ 2, {2, "Смарфон Xiaomi Redmi Note", 24999.50, 34, "Смартфоны"} },
+	{ 3, {3, "Наушники", 19999.99, 8, "Аксессуары"} },
+	{ 4, {4, "Монитор LG 328y547", 42999.95, 7, "Мониторы"} },
+
+};
+
+string processRequest(const string& request) {
+	if (request == "LIST_ALL") {
+		if (productDatabase.empty()) {
+			return "ERROR|DB is empty";
+		}
+
+		string response;
+
+		for (const auto& [id, product] : productDatabase) {
+			response += product.to_string_custom() + "\n";
+		}
+		return response;
+	}
+}
+
+void handleClient(int clientSocket) {
+	try {
+		char buffer[1024] = { 0 };
+		int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+		check_socket_error(bytesRead, "Ошибка чтения данных");
+
+		if (bytesRead <= 0) {
+			return;
+		}
+
+		string request(buffer, bytesRead);
+		cout << "Запрос получен " << request << endl;
+
+		string response = processRequest(request);
+
+		int byteSent = send(clientSocket, response.c_str(), response.size(), MSG_NOSIGNAL);
+		check_socket_error(byteSent, "Данные клиенту не отправлены.");
+		
+		cout << "Отправлен ответ: " << response.size() << " байт" << endl;
+
+	}catch(const exception& e){
+		cerr << "Ошибка обработки " << e.what() << endl;
+	}
+}
+
+int main() {
+
+
+	try {
+		//init_network();
+
+		int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+		check_socket_error(serverSocket, "Creatin socket error!");
+
+		sockaddr_in serverAddr;
+		serverAddr.sin_family = AF_INET;
+		serverAddr.sin_addr.s_addr = INADDR_ANY;
+		serverAddr.sin_port = htons(5555);
+
+		int opt = 1;
+
+	#ifdef _WIN32
+		check_socket_error(setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt)), "Installing error SO_REUSEDADDR");
+	#else
+		check_socket_error(setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)), "Installing error SO_REUSEDADDR");
+	#endif
+
+		check_socket_error(bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)), "bindin socket error!");
+		check_socket_error(listen(serverSocket, 10), "listening socket error!");
+
+		cout << "Server started on 5555 port" << endl;
+
+		while (true) {
+			sockaddr_in clientAddr;
+			socklen_t clientAddrLen = sizeof(clientAddr);
+			int clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientAddrLen);
+		
+			if (clientSocket == INVALID_SOCKET) {
+				cerr << "Ошибка принятия соединения" << endl;
+				continue;
+			}
+
+			char clientIP[INET_ADDRSTRLEN];
+			inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, INET_ADDRSTRLEN);
+			cout << "New connect from " << clientIP << ":" << ntohs(clientAddr.sin_port) << endl;
+			
+			// Функция обработки клиентских запросов
+			handleClient(clientSocket);
+			closesocket(clientSocket);
+
+			cout << "Соединение с клиентом закрыто" << endl;
+
+		}
+
+		close_socket(serverSocket);
+		cleanup_network();
+	}
+	catch (const exception& e) {
+		cerr << "Critical error: " << e.what() << endl;
+		cleanup_network();
+		return 1;
+	}
+
+	return 0;
+}
